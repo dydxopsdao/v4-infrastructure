c522a31c88716924322407ee75e2f4a7291b4e4d
Piotr Święs <10480513+pswies@users.noreply.github.com>
2023-12-19 12:22:26 +0100

    fix

--- message_signer/main.tf
+++ message_signer/main.tf
@@ -67,7 +67,7 @@ resource "aws_lambda_function" "sign_message" {
   handler       = "message_signer.run"
   role          = aws_iam_role.iam_for_lambda.arn
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  # source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.12"
 
