643e92680324e6834987ef1aed4161bb6219f65a
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-01-10 12:33:12 +0100

    Sleep after email sending due to AWS rate limits

--- validator_notifier/mailing.py
+++ validator_notifier/mailing.py
@@ -1,10 +1,15 @@
 # Code adapted from: https://docs.aws.amazon.com/ses/latest/dg/send-an-email-using-sdk-programmatically.html
 
+import time
+
 import boto3
 from botocore.exceptions import ClientError
 
 # The character encoding for the email.
 CHARSET = "UTF-8"
+
+# seconds; the AWS limit is 14 emails per second
+DELAY_PER_EMAIL = 0.08
 
 
 class Mailer:
@@ -12,7 +17,7 @@ class Mailer:
         self.client = boto3.client("ses", region_name=region)
         self.sender = sender
 
-    def send(self, subject: str, message: str, recipient: str):
+    def send(self, subject: str, message: str, recipient: str, delay=DELAY_PER_EMAIL):
         # Try to send the email.
         try:
             # Provide the contents of the email.
@@ -41,5 +46,5 @@ class Mailer:
         except ClientError as e:
             print(e.response["Error"]["Message"])
         else:
-            print("Email sent! Message ID:"),
-            print(response["MessageId"])
+            print(f"Email sent! Message ID: {response['MessageId']}. Waiting {delay} seconds.")
+            time.sleep(delay)
