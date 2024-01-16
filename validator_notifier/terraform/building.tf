75e2d19e09699e40c485f50385bc670942ce558e
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-01-16 18:49:32 +0100

    Fix permissions

--- validator_notifier/terraform/building.tf
+++ validator_notifier/terraform/building.tf
@@ -29,6 +29,7 @@ data "aws_iam_policy_document" "builder_permissions" {
       "logs:PutLogEvents",
       "ecr:*",
       "lambda:GetFunction",
+      "lambda:UpdateFunctionCode",
     ]
     resources = ["*"]
   }
