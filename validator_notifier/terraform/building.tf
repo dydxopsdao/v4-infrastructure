62373b84a4173ddc68f006940ec1287a89313e52
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-01-16 18:01:26 +0100

    Not lambda build

--- validator_notifier/terraform/building.tf
+++ validator_notifier/terraform/building.tf
@@ -108,8 +108,8 @@ resource "aws_codebuild_project" "validator_notifier" {
 
   environment {
     compute_type                = "BUILD_LAMBDA_1GB"
-    type                        = "LINUX_LAMBDA_CONTAINER"
-    image                       = "aws/codebuild/amazonlinux-x86_64-lambda-standard:python3.12"
+    type                        = "LINUX_CONTAINER"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
 
