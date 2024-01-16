5fa2580cbb80a6b33ac0aeab433850f0c163fb9f
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-01-16 15:45:07 +0100

    Start with a dummy image 2

--- validator_notifier/terraform/lambda.tf
+++ validator_notifier/terraform/lambda.tf
@@ -57,7 +57,7 @@ resource "aws_iam_role_policy_attachment" "lambda_permissions" {
 resource "aws_lambda_function" "notify_validators" {
   function_name    = "notify_validators"
   package_type     = "Image"
-  image_uri        = "public.ecr.aws/lambda/provided:latest" # Will be replaced by CodeBuild
+  image_uri        = "public.ecr.aws/lambda/python:3.11.2023.12.05.16-x86_64" # Dummy, will be replaced by CodeBuild
   role             = aws_iam_role.iam_for_lambda.arn
   timeout          = 90
   source_code_hash = timestamp() # Force update on every apply
