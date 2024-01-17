40f5b0b9e9f5db877c3613129381dcb0b9d12098
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-01-17 12:23:37 +0100

    Full automation

--- validator_notifier/terraform/build-image-with-codebuild.sh
+++ validator_notifier/terraform/build-image-with-codebuild.sh
@@ -11,13 +11,13 @@ eval "$(jq -r '@sh "REGION=\(.region)"')"
 # Trigger a build and wait for it to complete
 BUILD_ID=$(aws codebuild start-build --region=$REGION --project-name=validator-notifier | jq -r '.build.id')
 TIMEOUT=300
-while [ "$BUILD_PHASE" != '"COMPLETED"' ] && [ $TIMEOUT -gt 0 ]; do
+while [ "$BUILD_PHASE" != 'COMPLETED' ] || [ $TIMEOUT -le 0 ]; do
   sleep 5
   BUILD_PHASE=$(aws codebuild batch-get-builds --region=$REGION --ids=$BUILD_ID | jq -r '.builds[0].currentPhase')
   TIMEOUT=$((TIMEOUT-5))
 done
 
-"$BUILD_PHASE" != '"COMPLETED"' && exit 1
+"$BUILD_PHASE" != 'COMPLETED' && exit 1
 
 # Safely produce a JSON object containing the result value.
 # jq will ensure that the value is properly quoted
