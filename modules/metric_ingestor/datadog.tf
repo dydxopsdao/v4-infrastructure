4397aff314b07df8854b5643fbe792307ad7b26e
Piotr Święs <10480513+pswies@users.noreply.github.com>
2025-10-31 16:56:43 +0100

    chore: remove metric ingestor and related files

--- modules/metric_ingestor/datadog.tf
+++
@@ -1,8 +0,0 @@
-module "datadog_agent" {
-  source = "../datadog_agent"
-
-  env             = var.environment
-  datadog_api_key = var.datadog_api_key
-  dd_site         = var.datadog_site
-  service_name    = "metric-ingestor"
-}
